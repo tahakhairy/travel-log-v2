6c8906f5fa49d0c459a298dba8bc61513d3e33b9
Taha Khairy <tahakhairy@ymail.com>
2025-12-06 21:08:00 +0200

    :sparkles: login with github (#54)
    * :sparkles: login with github
    
    * :sparkles: add github vars to workflow

---
+++ app/components/auth-button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { loading: isLoading } = storeToRefs(useAuthStore());
+const { signIn } = useAuthStore();
+</script>
+
+<template>
+  <UButton
+    :loading="isLoading" :disabled="isLoading" label="Sign In With Github" trailing-icon="tabler:brand-github"
+    @click="signIn"
+  />
+</template>
